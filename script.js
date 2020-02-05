22388d1213e8b648ae39cd76d580326191e74a47
luan-nguyen20 <nguyenbaoluan1989@gmail.com>
2020-02-04 20:56:32 -0600

    add input filter

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+const input = document.querySelector(".input");
 
+//func to validate input for inputDisplay according to the inputFilter.
+function setInputFilter(input, inputFilter) {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+      input.addEventListener(event, function() {
+        if (inputFilter(this.value)) { //if inputFilter test succeeds
+            //set input value to new value
+            this.oldValue = this.value;
+            this.oldSelectionStart = this.selectionStart;
+            this.oldSelectionEnd = this.selectionEnd;
+        } 
+        else if (this.hasOwnProperty("oldValue")) {
+            //if test fails and input already has a value,
+            //set value to its old value (no changes)
+            this.value = this.oldValue;
+            this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+        } 
+        else { //if test fails and input doesn't have a value,
+            this.value = ""; //set value to empty
+        }
+      });
+    });
+}
+
+//integers >= 0
+const positiveIntNumOnlyRegEx = /^\d*$/;
+
+//func to check if a value has only int numbers
+function positiveIntNumOnlyTest(valueToTest){
+    return positiveIntNumOnlyRegEx.test(valueToTest);
+}
+
+//set the filter for inputDisplay to allow float numbers only
+setInputFilter(input, positiveIntNumOnlyTest);
