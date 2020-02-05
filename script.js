046386c0eef88459625a852580775497ab1ed66f
luan-nguyen20 <nguyenbaoluan1989@gmail.com>
2020-02-04 21:45:49 -0600

    working on say func

--- script.js
+++ script.js
@@ -33,3 +33,112 @@ function positiveIntNumOnlyTest(valueToTest){
 
 //set the filter for inputDisplay to allow float numbers only
 setInputFilter(input, positiveIntNumOnlyTest);
+
+const sayStr = document.querySelector("#sayStr");
+
+let resultStr, left, write;
+
+function say(num){
+    resultStr = '';
+    if (num === 0){resultStr = "Zero.";}
+
+    onesPlace = ['one','two','three','four','five','six','seven','eight','nine'];
+    tensPlace = ['ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+    teens = ['eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+
+    left = num; //what we still have left to write out
+    write = left / 1000000000000; //how many trillions left to write out
+    left = left - write*1000000000000; //subtract those trillions
+
+    if (write > 0){
+        trillions = say(write);
+        resultStr = resultStr + trillions + " trillion";
+    
+        if (left > 0){
+            resultStr = resultStr + " "; //so we don't write 'two trilliontwo billion'
+        }
+    }
+
+    write = left / 1000000000; //how many billions left to write out
+    left = left - write*1000000000; //subtract those billions
+
+    if (write > 0){
+        billions = say(write);
+        resultStr = resultStr + billions + " billion";
+    
+        if (left > 0){
+            resultStr = resultStr + " "; //so we don't write 'two billiontwo million'
+        }
+    }
+    
+    write = left / 1000000; //how many millions left to write out
+    left = left - write*1000000; //subtract those millions
+
+    if (write > 0){
+        millions = say(write);
+        resultStr = resultStr + millions + " million";
+
+        if (left > 0){
+            resultStr = resultStr + " "; //so we don't write 'two milliontwo thousand'
+        }
+    }
+    
+    write = left / 1000; //how many thousands left to write out
+    left = left - write*1000; //subtract those thousands
+
+    if (write > 0){
+        thousands = say(write);
+        resultStr = resultStr + thousands + " thousand";
+
+        if (left > 0){
+            resultStr = resultStr + " "; //so we don't write 'two thousandtwo hundred'
+        }
+    }
+
+    write = left/100; //how many hundreds left to write out
+    left = left - write*100; //subtract those hundreds
+
+    if (write > 0){
+        hundreds = say(write);
+        resultStr = resultStr + hundreds + " hundred";
+        
+        if (left > 0){
+            resultStr = resultStr + " and "; //so we don't write 'two hundredfifty-one'
+        }
+    }
+
+    write = left/10; //how many tens left to write out
+    left  = left - write*10;  // Subtract off those tens.
+
+    if (write > 0){
+        if ((write == 1) && (left > 0)){
+            //special exceptions for teens
+            resultStr = resultStr + teens[left-1];
+            left = 0;
+        }
+        else{
+            resultStr = resultStr + tensPlace[write-1];
+        }
+
+        if (left > 0){
+            resultStr = resultStr + "-"; //so we don't write 'sixtyfour'
+        }
+    }
+
+    write = left; //hows many ones left to write out
+    left = 0; //subtract those ones
+
+    if (write > 0){
+        resultStr = resultStr + onesPlace[write-1];
+    }
+
+    sayStr.textContent = resultStr;
+    return resultStr;
+}
+
+//event listener for submitBtn
+const submitBtn = document.querySelector("#submitBtn")
+submitBtn.addEventListener('click',function() {
+    say(Number(input.value));
+});
+
